8a9966b9f0ba305d682e83344b2c6852154e15a6
Kent Delante <kdeleteme@tutanota.com>
2019-03-31 23:06:00 +0800

    Setup top panel

---
+++ src/components/Panel.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="panel">
+  <div class="activities">
+    <p>Activities</p>
+  </div>
+  <div class="time">
+    <p>Sun 22:00</p>
+  </div>
+  <div class="user-menu">
+    <i class="fa fa-wifi"></i>
+    <i class="fa fa-volume-up"></i>
+    <i class="fa fa-power-off"></i>
+    <i class="fa fa-caret-down"></i>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Panel extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.panel {
+    background-color: #000000;
+    font-weight: 700;
+    color: #cccccc;
+    height: 27px;
+    min-width: 720px;
+    width: 100vw;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+
+    .activities {
+        margin-left: 12px;
+    }
+
+    .fa {
+        margin-right: 12px;
+    }
+}
+h2 {
+    margin: 0;
+    padding: 0;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import 'normalize.css';
+import 'fork-awesome/scss/fork-awesome.scss';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+    store,
+    render: (h) => h(App),
 }).$mount('#app');
