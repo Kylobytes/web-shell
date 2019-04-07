2bce1757b65d63d121318bb866240482c8fd116e
Kent Delante <kdeleteme@tutanota.com>
2019-04-07 18:01:47 +0800

    Add background image and Cantarell font

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,20 @@
 </template>
 
 <script lang="ts">
-import Panel from './components/Panel.vue';
+import Panel from './components/Panel.vue'
+
+import 'typeface-cantarell'
+
+
 export default {
     components: { Panel },
-};
+}
 </script>
 
 <style lang="scss">
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    background: url('./assets/kylobytes-material.png') grey center;
+    font-family: 'Cantarell', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
@@ -26,14 +31,4 @@ export default {
     width: 100vw;
     height: 100vh;
 }
-/*#nav {
-    padding: 30px;
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
-}*/
 </style>
