b205af0f3639c68b8727de172905aa6847ceda46
Kent Delante <kdeleteme@tutanota.com>
2020-11-02 19:44:45 +0800

    Remove fork awesome and adwaita-icon-theme submodule

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import 'normalize.css'
-import 'fork-awesome/scss/fork-awesome.scss'
 
 Vue.config.productionTip = false
 
